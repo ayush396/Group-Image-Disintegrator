02d15321a35928ed4f97647290c34268325818d8
Ayush Pushkarna <83595522+ayush396@users.noreply.github.com>
2021-12-14 19:36:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template,request
 import os
+from extrator import extract
 from werkzeug.utils import secure_filename
 app=Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -11,7 +13,8 @@ def index():
 def upload():
     if request.method=='POST':
         f=request.files['inp_file']
-        f.save(os.path.join("env//static",secure_filename(f.filename)))
+        f.save(os.path.join("env//static",secure_filename('upload.jpg')))
+        extract.faces()
         return "Upload Successfully"
     
 if __name__ == '__main__':
