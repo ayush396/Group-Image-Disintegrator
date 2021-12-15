d53af3e162f1020757ee74b7ca6fe0dbdfade7f5
Ayush Pushkarna <83595522+ayush396@users.noreply.github.com>
2021-12-15 22:23:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,send_file,request
 import os
 from extrator import extract
 from werkzeug.utils import secure_filename
 
 from seperator import seperate
+
+import shutil
 
 app=Flask(__name__)
 
@@ -19,7 +21,19 @@ def upload():
         f.save(os.path.join("env//static",secure_filename('upload.jpg')))
         extract.faces()
         seperate.segregator()
-        return "Upload Successfully"
-    
+        shutil.make_archive('env\segregatorMale','zip','env\Male')
+        shutil.make_archive('env\segregatorFemale','zip','env\Female')
+        return render_template("upload.html")
+
+@app.route('/download')
+def segregatorMale():
+    p='segregatorMale.zip'
+    return send_file(p,as_attachment=True)
+
+@app.route('/downloadF')
+def segregatorFemale():
+    p='segregatorFemale.zip'
+    return send_file(p,as_attachment=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
