15b51ad3ffdf9fff06e90535652e6081836ecfcd
Shrujal21 <88179449+Shrujal21@users.noreply.github.com>
2021-12-15 14:35:34 +0530

    Update genderDetect.py

--- genderDetect.py
+++ genderDetect.py
@@ -101,6 +101,6 @@ adam1=Adam(lr=0.001,decay=0.001/50)
 model.compile(loss='binary_crossentropy',optimizer=adam1,metrics=['accuracy'])
 
 fit_model=model.fit_generator(augmented.flow(X_train,Y_train,batch_size=64),validation_data=(X_test,Y_test),steps_per_epoch=size//64,epochs=50,verbose=1)
-model.save('GenderClassifier.model')
+model.save('env//GenderClassifier.model')
 
 
