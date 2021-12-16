19c1505181dd15ca4a40346b02d828e60dce7091
Ayush Pushkarna <83595522+ayush396@users.noreply.github.com>
2021-12-16 15:31:47 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,7 +1,14 @@
 const srcImg = document.getElementById('src-image');
 const fileInput = document.getElementById('input-file');
 
+
+function changeStyle() {
+    var element = document.getElementById("src-image");
+    element.style.opacity = "1";
+}
+
 fileInput.addEventListener('change', e => {
     srcImg.src = URL.createObjectURL(e.target.files[0]);
+    changeStyle()
 
 }, false);
