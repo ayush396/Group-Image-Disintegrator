d339e14e46f3c8350b05a86dcde5bce5a6b4fa3c
Ayush Pushkarna <83595522+ayush396@users.noreply.github.com>
2021-12-18 01:20:14 +0530

    Update seperator.py

--- seperator.py
+++ seperator.py
@@ -4,12 +4,11 @@ import numpy as np
 
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-import shutil
+
 
 model=load_model('env\GenderClassifier.model')
 prediction=[]
-input=os.listdir('env//extracted_pics')
-
+allfiles=[]
 
 classes=['Male','Female']
 #print(input[8])
@@ -20,12 +19,11 @@ class seperate():
         pass
     
     def segregator():
-        shutil.rmtree('env//Female')
-        shutil.rmtree('env//Male')
-        os.mkdir('env//Male')
-        os.mkdir('env//Female')
+        input=os.listdir('env//extracted_pics')
+        # files=os.listdir('env//extracted_pics')
         for x in input:
             img=cv2.imread('env//extracted_pics//'+x)
+            print(x)
             img=cv2.resize(img,(96,96))
             img=img.astype('float')/255.0
 
@@ -34,15 +32,17 @@ class seperate():
 
             pred=model.predict(img)[0]
             #print(pred[0])
-            if pred[0]>0.95:
+            if pred[0]>0.5:
                 idx=0
             else:
                 idx=1
             label=classes[idx]
             prediction.append(label)
-
+                 
             #print(prediction)
             
+            #print(files)
+
 
             # print(len(prediction))
             for i in range(len(prediction)):
@@ -50,7 +50,27 @@ class seperate():
                     im=cv2.imread('env//extracted_pics//'+input[i])
                     path='env//Female//'+input[i]
                     cv2.imwrite(path,im)
+                    border = cv2.copyMakeBorder(im, 7, 7, 7, 7, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                    cv2.imwrite('env//border//output_'+ input[i] , border)
+
                 else:
                     img=cv2.imread('env//extracted_pics//'+input[i])
                     path='env//Male//'+input[i]
                     cv2.imwrite(path,img)
+                    border = cv2.copyMakeBorder(img, 7, 7, 7, 7, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                    cv2.imwrite('env//border2//output_'+ input[i] , border)
+
+     
+        print(len(prediction))
+
+
+
+
+
+
+
+
+    
+
+
+      
